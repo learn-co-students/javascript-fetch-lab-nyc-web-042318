00ac1e55f6cf6bc11a37da5ebdf058340a05f831
Betsy S <32912203+ensallee@users.noreply.github.com>
2018-06-10 14:59:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -60,6 +60,5 @@ function forkRepo() {
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  // return 'adae6738076ba9b130029e2ef31414d80e22d1dd'
   return ''
 }
